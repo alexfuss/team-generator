5d71d7ec3b41102e4ba013c71dd085fcf1d464c4
Alex Fuss <alexfuss2@gmail.com>
2020-08-03 20:32:33 -0400

    Still working to update script. Email is showing as undefined.

--- Proj_files/Develop/lib/Employee.js
+++ Proj_files/Develop/lib/Employee.js
@@ -1,1 +1,37 @@
 // TODO: Write code to define and export the Employee class
+class Employee {
+    // Create a constructor that defines the information within the Employee class
+
+    constructor(name, id, email) {
+        if(typeof name !== "string" || !name.trim().length) {
+            throw new Error("Name should not be an empty string.");
+        }
+
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+
+    // The below functions will grab all the employee information
+    getName() {
+        console.log(`Employee Name: ${this.name}`);
+
+        return this.name;
+    };
+
+    getId() {
+        console.log(`Employee ID: ${this.id}`);
+
+        return this.id;
+    };
+
+    getEmail() {
+        console.log(`Employee Email: ${this.email}`);
+    };
+
+    getRole() {
+        return "Employee";
+    }
+}
+
+module.exports = Employee;
